45bd416a4c4dab1f2a6c2ffdc7de3675cde5ee3d
Skyla <skylaclark094@gmail.com>
2024-02-21 00:36:52 -0500

    Create the Team enum

---
+++ src/main/java/me/teamone/helpers/Team.java
@@ -0,0 +1,34 @@
+package me.teamone.helpers;
+
+import javafx.scene.paint.Color;
+
+/**
+ * Enum for teams.
+ *
+ * Used to keep consistency when comparing teams, and to easily store any hardcoded team data.
+ */
+public enum Team {
+    WHITE(Color.WHITE),
+    BLACK(Color.BLACK);
+
+    /**
+     * The color used to represent the team.
+     */
+    private final Color color;
+
+    /**
+     * Team constructor
+     * @param color the color to make the team.
+     */
+    Team(Color color) {
+        this.color = color;
+    }
+
+    /**
+     * Get the selected teams color.
+     * @return The color of the team.
+     */
+    public Color getColor() {
+        return color;
+    }
+}
