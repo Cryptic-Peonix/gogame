70f50c037f6060b96cc0f7ea936657f89ae94e84
Skyla <skylaclark094@gmail.com>
2024-02-21 15:12:53 -0500

    add number to stone

--- src/main/java/me/teamone/gogame/core/gameobjects/Stone.java
+++ src/main/java/me/teamone/gogame/core/gameobjects/Stone.java
@@ -9,9 +9,19 @@ import me.teamone.gogame.core.helpers.Team;
 public class Stone {
 
     private final Team team;
+    /**
+     * Int to distinguish stones.
+     * Must differentiate so stones for player are not all one reference to the same obj.
+     */
+    private final int number;
 
-    public Stone(Team team) {
+    public Stone(Team team, int number) {
         this.team = team;
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
     }
 
     public Team getTeam() {
