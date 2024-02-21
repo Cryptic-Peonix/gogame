cbc0643614a0db6576536160bcb44d18bf125ad3
Skyla <skylaclark094@gmail.com>
2024-02-21 15:02:26 -0500

    create StonePlacementException

---
+++ src/main/java/me/teamone/gogame/core/exceptions/StonePlacementException.java
@@ -0,0 +1,11 @@
+package me.teamone.gogame.core.exceptions;
+
+/**
+ * Exception to be thrown when there is an error with stone placement
+ */
+public class StonePlacementException extends Exception {
+
+    public StonePlacementException(String m) {
+        super(m);
+    }
+}
