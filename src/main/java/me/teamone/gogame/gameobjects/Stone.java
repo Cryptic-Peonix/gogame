1e097f1beba123a399eb68448bbffcf045e73f5e
Skyla <skylaclark094@gmail.com>
2024-02-21 00:36:26 -0500

    Create the Stone class

---
+++ src/main/java/me/teamone/gogame/gameobjects/Stone.java
@@ -0,0 +1,20 @@
+package me.teamone.gogame.gameobjects;
+
+import me.teamone.helpers.Team;
+
+/**
+ * Stone object.
+ * Used to represent a stone in game.
+ */
+public class Stone {
+
+    private final Team team;
+
+    public Stone(Team team) {
+        this.team = team;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+}
