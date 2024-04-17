d463fdfb85afdb01b7ececaeb225d99ed9324828
Skyla <skylaclark094@gmail.com>
2024-04-17 14:01:28 -0400

    Add StringCreationException

---
+++ src/main/java/me/teamone/gogame/core/exceptions/StringCreationException.java
@@ -0,0 +1,11 @@
+package me.teamone.gogame.core.exceptions;
+
+/**
+ * Exception for string creation errors.
+ */
+public class StringCreationException extends Exception {
+
+    public StringCreationException(String m) {
+        super(m);
+    }
+}
